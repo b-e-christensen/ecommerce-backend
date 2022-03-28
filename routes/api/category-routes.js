8cfa4ba9e55158bcd3adaccf40f9b66cbc39a607
Ben Christensen <ben.eric.christensen@gmail.com>
2022-03-28 08:23:44 -0600

    cleaned up comments on code after all routes seem to be working. final commit?

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,10 +4,7 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 // GET all categories (with associated products)
-// ******* THIS ROUTE WORKS *******
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   try {
   const categoryData = await Category.findAll({
     include: [{ model: Product }],
@@ -19,10 +16,7 @@ router.get('/', async (req, res) => {
 });
 
 // GET a single category based on id (with associated products)
-// ******* THIS ROUTE WORKS *******
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
   const categoryData = await Category.findByPk(req.params.id, {
     include: [{ model: Product }],
@@ -37,9 +31,8 @@ router.get('/:id', async (req, res) => {
 }
 });
 
-// ******* THIS ROUTE WORKS *******
-router.post('/', async (req, res) => {
   // create a new category
+router.post('/', async (req, res) => {
   try {
     const categoryData = await Category.create(req.body)
     res.status(200).json(categoryData)
@@ -48,9 +41,8 @@ router.post('/', async (req, res) => {
   }
 });
 
-// ******* THIS ROUTE WORKS *******
+// update a category by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
   try {
     const categoryData = await Category.update({ category_name: req.body.category_name }, {
       where: {
@@ -68,9 +60,8 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-// ******* THIS ROUTE WORKS *******
+// delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
       where: {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,10 +4,8 @@ const { update } = require('../../models/Category');
 
 // The `/api/tags` endpoint
 
-// ******* THIS ROUTE WORKS *******
+// find all tags
 router.get('/', async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
       include: [{ model: Product }]
@@ -18,10 +16,8 @@ router.get('/', async (req, res) => {
   }
 });
 
-// ******* THIS ROUTE WORKS *******
+// find a single tag by its `id`
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }]
@@ -32,9 +28,8 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// ******* THIS ROUTE WORKS *******
+// create a new tag
 router.post('/', async (req, res) => {
-  // create a new tag
   try {
   const tagData = await Tag.create(req.body)
   res.status(200).json(tagData)
@@ -43,9 +38,8 @@ router.post('/', async (req, res) => {
   }
 });
 
-// ******* THIS ROUTE WORKS *******
+// update a tag's name by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
   try {
     const tagData = await Tag.update({ tag_name: req.body.tag_name }, {
       where: {
@@ -63,9 +57,8 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-// ******* THIS ROUTE WORKS *******
+// delete on tag by its `id` value
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
   try {
     const tagData = await Tag.destroy({
       where: {
